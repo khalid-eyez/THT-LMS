ab8fc6a090caf928ccc6e689cead60bf49ec0e96
khalid-eyez <OneCampus2019@gmail.com>
2026-02-14 11:32:40 +0300

    Loan processing fixes

--- frontend/loans_module/views/loans/pending_applications.php
+++ frontend/loans_module/views/loans/pending_applications.php
@@ -256,7 +256,7 @@ $this->registerCss($css);
                           <td><?= Html::encode($createdAt) ?></td>
                           <td><?= Html::encode($updatedAt) ?></td>
 
-                          <td class="text-center action-btns">
+                          <td class="action-btns">
                              <a href="<?= $viewUrl($row->id) ?>" title="View Loan" class="btn btn-xs btn-primary">
                                 <i class="fa fa-eye"></i>
                              </a>
